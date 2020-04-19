8c87f38f2b8bb1c6175b4085e4b47e67611364ad
Vršek, Daniel <vrsek@havit.cz>
2020-04-19 18:37:42 +0200

    Add project ServerSide.Core

---
+++ src/ServerSide.Core/wwwroot/js/appInitializer.js
@@ -0,0 +1,18 @@
+﻿window.appInitializer = {
+    "callbacks": [],
+    "subscribe": null,
+    "onAppLoaded": null
+}
+
+window.appInitializer.callbacks = [];
+
+window.appInitializer.subscribe = function(callback) {
+    window.appInitializer.callbacks.push(callback);
+}
+
+window.appInitializer.onAppLoaded = function () {
+    var callbacks = window.appInitializer.callbacks;
+    for (var i = 0; i < callbacks.length; i++) {
+        callbacks[i]();
+    }
+}
