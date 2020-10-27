acb4d1e25813823d4003b0f1f2553747678921e9
Vršek, Daniel <vrsek@havit.cz>
2020-10-27 15:30:15 +0100

    Add blazor-mobx projects to solution

---
+++ src/Shared/Models/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Havit.Blazor.Mobx.Samples.Models
+{
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+    }
+}
