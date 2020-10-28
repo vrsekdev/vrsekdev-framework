211c2e6c38fb13b86f7bebc4499a699149ef1d4c
Vršek, Daniel <vrsek@havit.cz>
2020-10-28 13:57:57 +0100

    Add project Blazor.Templates.Frest

---
+++ src/Blazor.Templates.Frest/Shared/FrestLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VrsekDev.Blazor.Templates.Frest.Shared
+{
+    public partial class FrestLayout<TNavBar, TLogo, TMainMenu, TFooter>
+        where TNavBar : IComponent
+        where TLogo : IComponent
+        where TMainMenu : IComponent
+        where TFooter : IComponent
+    {
+    }
+}
